7b7ba30ecc4f4b92882b8638be7362362f2da76d
mbedded <github@mbuege.com>
2025-11-03 20:43:47 +0100

    feat: Add components like spinner or error

---
+++ src/views/Error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let header: string;
+  export let message: string;
+</script>
+
+<style>
+  .error-container {
+    border: 1px solid var(--background-modifier-border);
+  }
+
+  .error-header {
+    color: var(--text-error);
+    font-weight: bold;
+  }
+</style>
+
+<div class="error-container">
+  <div class="error-header">{header}</div>
+  <div>{message}</div>
+</div>
---
+++ src/views/Spinner.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  export let text: string;
+</script>
+
+<style>
+  .container {
+    display: flex;
+    align-items: center;
+    margin-top: 10px; /* Just for some spacing */
+  }
+
+  .container span {
+    margin-left: 10px; /* Space between spinner and text */
+  }
+
+  .spinner {
+    display: inline-block;
+    width: 20px; /* Adjust size as needed */
+    height: 20px; /* Adjust size as needed */
+    /* We're using obsidian colors here */
+    border: var(--interactive-normal);
+    border-top: 4px solid var(--interactive-accent); /* Accent color */
+    border-radius: 50%;
+    animation: spin 1.5s linear infinite;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
+
+<div class='container'>
+  <div class="spinner"></div>
+  <span>{text}</span>
+</div>
