cd9f004f449380402b4b64ab3844109fc6e70eed
mbedded <github@mbuege.com>
2025-11-05 16:59:10 +0100

    feat: Update settings to generate token for basic authentication

--- src/settings/Settings.ts
+++ src/settings/Settings.ts
@@ -1,11 +1,17 @@
 export interface TracksPluginSettings {
-	tracksUrl: string;
-	tracksUsername: string;
-	tracksToken: string;
+  tracksUrl: string;
+  tracksUsername: string;
+  tracksToken: string;
+
+  getBasicToken(): string;
 }
 
 export const DEFAULT_SETTINGS: TracksPluginSettings = {
-	tracksUrl: "",
-	tracksUsername: "",
-	tracksToken: "",
+  tracksUrl: "",
+  tracksUsername: "",
+  tracksToken: "",
+  getBasicToken(): string {
+    const basic = `${this.tracksUsername}:${this.tracksToken}`;
+    return btoa(basic)
+  },
 }
