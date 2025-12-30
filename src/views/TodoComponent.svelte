28f3e43d9c5ddaca074041ffb1732260674fe14f
mbedded <github@mbuege.com>
2025-12-30 15:12:33 +0100

    refactor: Rename method and tweak styles in TodoComponent
    - Renamed `markDone` method to `OnClickDone` for clarity.
    - Adjusted CSS styles and added comments for future improvements.
    - Updated button text to a checkmark symbol for a better UI.

--- src/views/TodoComponent.svelte
+++ src/views/TodoComponent.svelte
@@ -2,7 +2,9 @@
   import { TodoItem } from "../adapters/TodoClasses";
 
   interface Props {
+    // The entry to display and interact with.
     todo: TodoItem;
+    // Function (callback to parent) to mark an entry as done.
     markTodoAsDone: (x: TodoItem) => Promise<boolean>
   }
 
@@ -11,13 +13,8 @@
     markTodoAsDone
   }: Props = $props();
 
-  let waiting = $state(false);
-
-  async function markDone() {
-    waiting = true;
-    const results = await markTodoAsDone(todo);
-
-    waiting = false;
+  async function OnClickDone() {
+    const success = await markTodoAsDone(todo);
   }
 </script>
 
@@ -29,20 +26,25 @@
   .btn-done {
     background-color: var(--interactive-accent);
     color: var(--text-on-accent);
+    /* todo: check or delete style */
+    /*background-color: var(--interactive-normal);*/
+    /*color: var(--text-normal);*/
   }
 
   .description {
+    margin-left: 5px;
+    vertical-align: center;
     /* todo: align vertical center */
   }
 </style>
 
 <div class="container">
-  <button type="button" class="btn-done" onclick={markDone}>
+  <button type="button" class="btn-done" onclick={OnClickDone}>
     <!-- todo: add icon or something-->
     <!--    <Check/>-->
-    Done
+    ✓
   </button>
 
-  <span class="description">{todo.description}</span>ˆ
+  <span class="description">{todo.description}</span>
 </div>
 
